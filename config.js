7af4b1a3474bdf0457bfe16ffa3b695799e251eb
nicholasjohnjoseph-prog <nicholasjohnjoseph@gmail.com>
2026-02-05 17:33:26 -0600

    config.js
    Refactor API settings to use a single API_KEY variable.

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
-const API_SETTINGS = {
-    KEY: '96e49f1454195e418c84e5f6' // Put your actual key here
-};
+// config.js
+const API_KEY =
+    '96e49f1454195e418c84e5f6';
+// Or if using an object: 
+// window. CONFIG = { API_KEY:
+'96e49f1454195e418c84e5f6' };
